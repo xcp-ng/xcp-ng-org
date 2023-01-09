a5c8c6c9b1e3175771d8109b7aa0ae0b01fe9a52
Olivier Lambert <olivier.lambert@vates.fr>
2023-01-09 22:00:50 +0100

    Improve edit link to target the current branch
    Signed-off-by: Olivier Lambert <olivier.lambert@vates.fr>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/xcp-ng/xcp-ng-org/edit/master/',
+            'https://github.com/xcp-ng/xcp-ng-org/edit/olt-new-doc/',
         },
         blog: false,
         theme: {
