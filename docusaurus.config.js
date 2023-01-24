75d6bf23301f0fb6693a4d8cb1dfb1072195c5cd
Olivier Lambert <olivier.lambert@vates.fr>
2023-01-24 16:17:04 +0100

    Removing useless Docusaurus parameters
    Signed-off-by: Olivier Lambert <olivier.lambert@vates.fr>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,8 +17,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'xcp-ng', // Usually your GitHub org/user name.
-  projectName: 'xcp-ng-org', // Usually your repo name.
+  //organizationName: 'xcp-ng', // Usually your GitHub org/user name.
+  //projectName: 'xcp-ng-org', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
