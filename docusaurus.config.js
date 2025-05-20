905095fdf1673374497679ca7a012e812f4dbe19
Thomas Moraine <thomas.moraine@vates.tech>
2025-05-20 08:16:52 +0200

    doc(XCP-ng): Fix wrong GitHub link in footer
    In the XCP-ng documentation, the GitHub link in the footer was the default one used by the Docusaurus template.
    
    This commit changes the link so that it points to the XCP-ng GitHub repository instead.
    
    Signed-off-by: Thomas Moraine <thomas.moraine@vates.tech>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,7 +137,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/xcp-ng/xcp-ng-org',
               },
             ],
           },
